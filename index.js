4b46557ff34bfdeb170167c4b88cc523e2c1e347
fatima abdullah <fatimajubary@gmail.com>
2023-01-01 23:23:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 
-// Write your code here
+let num1=8 , num2=8, num3=16, num4=6;
+let multiply = (num1 * num2) -2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3%num4;
+let max = Math.max(1,14,20);
